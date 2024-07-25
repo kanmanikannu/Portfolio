fd4fd51ec69082f55b46af291c09c867cfd23693
kanmani <kanmanikannu117@gmail.com>
2024-07-25 09:59:06 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <section id="home" className="text-center container">
+            <h1>Welcome to My Portfolio</h1>
+            <p>Hi, I'm Kanmani. A passionate developer from Coimbatore.</p>
+        </section>
+    );
+}
+
+export default Home;
